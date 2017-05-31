c77a0fad0e7c73caf428d2bb520b0f56b122ba16
sp00m <christophe.maillard@rocketmail.com>
2017-05-31 17:18:38 +0100

    [1.3.0] Disable caching on index.html

--- server/server.js
+++ server/server.js
@@ -1,8 +1,9 @@
 /* eslint-disable no-console, no-magic-numbers */
 
 const express = require("express");
+const path = require("path");
 const info = require("./info");
-const production = "production" === process.env.NODE_ENV;
+const basedir = "production" === process.env.NODE_ENV ? "dist" : "public";
 
 express()
 
@@ -10,7 +11,14 @@ express()
   .get("/info/gen", (request, response) => response.json(info.gen()))
   .get("/info/poll", (request, response) => response.json(info.poll()))
 
-  .use("/", express.static(production ? "dist" : "public", { maxage: "1y" }))
+  .use("/", express.static(basedir, {
+    maxage: "1y",
+    setHeaders: (response, filePath) => {
+      if (filePath === path.join(__dirname, `../${basedir}`, "index.html")) {
+        response.setHeader("Cache-Control", "no-cache, no-store");
+      }
+    }
+  }))
 
   .listen(process.env.NODE_PORT || 3000, process.env.NODE_IP || "localhost", () => {
     console.log(`Application worker ${process.pid} started...`);
