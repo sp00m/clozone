fa4e92b71fb0512213496b86c424801f075cc1de
sp00m <christophe.maillard@rocketmail.com>
2017-05-31 19:58:58 +0100

    [1.4.0] Adding version

--- public/modules/game/Dimensions.domain.js
+++ public/modules/game/Dimensions.domain.js
@@ -1,14 +1,14 @@
 angular.module("game")
 
-.factory("game.Dimensions", ["game.X_SCALE", "game.Y_SCALE", "game.MARGIN",
-function (X_SCALE, Y_SCALE, MARGIN) { // eslint-disable-line indent
+.factory("game.Dimensions", ["game.X_SCALE", "game.Y_SCALE", "game.X_MARGIN", "game.Y_MARGIN",
+function (X_SCALE, Y_SCALE, X_MARGIN, Y_MARGIN) { // eslint-disable-line indent
 
   return class Dimensions {
 
     constructor(width, height) {
       /* eslint-disable no-magic-numbers */
-      this.xMin = Math.round(width * MARGIN * -1 - X_SCALE / 3);
-      this.yMin = Math.round(height * MARGIN * -1 - Y_SCALE / 3);
+      this.xMin = Math.round(width * X_MARGIN * -1 - X_SCALE / 3);
+      this.yMin = Math.round(height * Y_MARGIN * -1 - Y_SCALE / 3);
       this.width = width + this.xMin * -2;
       this.height = height + this.yMin * -2;
       this.ratio = this.height / this.width;
--- public/modules/game/game.module.js
+++ public/modules/game/game.module.js
@@ -3,5 +3,6 @@ angular.module("game", ["ngAnimate", "generator", "utils"])
 /* eslint-disable no-magic-numbers */
 .constant("game.X_SCALE", 112 / 2)
 .constant("game.Y_SCALE", 97)
-.constant("game.MARGIN", 5 / 100);
+.constant("game.X_MARGIN", 5 / 100)
+.constant("game.Y_MARGIN", 0 / 100);
 /* eslint-enable no-magic-numbers */
