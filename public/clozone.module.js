2db15f68ecbddea5b7652fa1c4e2752228b4b761
sp00m <christophe.maillard@rocketmail.com>
2017-06-14 07:43:44 +0100

    [2.0.0] v added to

--- public/clozone.module.js
+++ public/clozone.module.js
@@ -3,15 +3,16 @@ angular.module("clozone", ["ui.router", "home", "game"])
 .constant("game.COLOR1", "#008F95")
 .constant("game.COLOR2", "#E9B000")
 
-.run(["$rootScope", "$window", "game.COLOR1", "game.COLOR2",
-function ($rootScope, $window, COLOR1, COLOR2) { // eslint-disable-line indent
+.run(["$rootScope", "$window", "game.COLOR1", "game.COLOR2", "utils.v",
+function ($rootScope, $window, COLOR1, COLOR2, v) { // eslint-disable-line indent
 
   "use strict";
 
   Object.assign($rootScope, {
     version: $window.clozone.version,
     color1: COLOR1,
-    color2: COLOR2
+    color2: COLOR2,
+    v
   });
 
   $rootScope.$on("$stateChangeSuccess", (event, toState) => {
